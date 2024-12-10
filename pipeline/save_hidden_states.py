6628af5d8169fb960af831d6ce59a72ec4f935b4
thanhvu <thanh1908.xx@gmail.com>
2024-12-10 15:41:54 +0700

    update

--- pipeline/save_hidden_states.py
+++ pipeline/save_hidden_states.py
@@ -107,6 +107,8 @@ def get_generations(model_name:str, args, seed=1, old_sequences=None, max_num_ge
         stop_words = []
     if args.fraction_of_data_to_use < 1.0:
         dataset = dataset.train_test_split(test_size=(1 - args.fraction_of_data_to_use), seed=seed)['train']
+
+    dataset = sorted(dataset, key=lambda x: len(x['prompt']), reverse=True)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False)
     print('len dataset', len(dataloader))
     if old_sequences is None:
